d9b1989b4ecbfea9398ded349ca35c8c4bf05108
bluemix trial <bluemix.trial.101@gmail.com>
2017-01-19 09:14:37 -0600

    default host

--- server.py
+++ server.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
     configure_logger(level=logging.DEBUG)
 
     parser = argparse.ArgumentParser(description='Echo CherryPy Server')
-    parser.add_argument('--host', default='127.0.0.1')
+    parser.add_argument('--host', default='')
     parser.add_argument('-p', '--port', default=9000, type=int)
     parser.add_argument('--ssl', action='store_true')
     args = parser.parse_args()
